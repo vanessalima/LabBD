b17890eefe0cc954975f05030146a0ff92fbbe99
Vanessa Lima <vanessalima@Vanessas-MacBook-Pro.local>
2014-09-09 14:08:21 -0300

    Adicionando linhas.

--- scriptCria.sql
+++ scriptCria.sql
@@ -17,8 +17,8 @@ ALTER SESSION SET NLS_LANGUAGE= 'PORTUGUESE' NLS_TERRITORY= 'BRAZIL'
 ALTER SESSION SET deferred_segment_creation = FALSE;
 /
 -- Remoção das Tabelas
-DROP TABLE emissora CASCADE CONSTRAINT;
-DROP TABLE nome CASCADE CONSTRAINT;
+DROP TABLE evento CASCADE CONSTRAINT;
+DROP TABLE edicao CASCADE CONSTRAINT;
 /
 -- Remoção das Sequências
 DROP SEQUENCE SEQ_IDDE_DEPTO;
@@ -30,33 +30,32 @@ DROP SEQUENCE SEQ_IDDE_DEPTO;
  *	nomeEv é o nome do evento e a também compõe a chave primária desta tabela
  *	descricaoEv contém um texto descrevendo brevemente o evento, tais como a 
  área de interesse
- *	websiteEv é o site do evento no formato *** ???
+ *	websiteEv é o site do evento que deve iniciar no formato http:// ou https://
  *	totalArtigosApresentadosEv é um atributo derivado que armazena a 
  quantidade total de artigos apresentados no evento em todas as suas edição
  */
-CREATE TABLE EVENTO (
-	codEv Number(10) NOT NULL,	-- Chave primária
+CREATE TABLE evento (
+	codEv Number(5) NOT NULL,	-- Chave primária
 	nomeEv Varchar2(100) NOT NULL, -- Chave primária
-	descricaoEv Varchar2(400),
+	descricaoEv Varchar2(1000),
 	websiteEv Varchar2(200),
 	totalArtigosApresentadosEv Number(100), -- Atributo derivado ***
 	-- *** outros constraints ??? 
 	CONSTRAINT PK_EVENTO PRIMARY KEY (codEv, nomeEv) -- PK_EVENTO define a restrição de chave primária
-	-- 
 );
 /
 -- Criação da tabela Edição
 /*
  * Esta tabela comtém os dados de uma edição de um dado evento no qual:
- *	codEv é uma chave estrangeira da tabela EVENTO e que é utilizada 
+ *	codEv é uma chave estrangeira da tabela evento e que é utilizada 
  também como chave primária, devido a relação de entidade-fraca
- *	nomeEv é uma chave estrangeira da tabela EVENTO e que é utilizada 
+ *	nomeEv é uma chave estrangeira da tabela evento e que é utilizada 
  também como chave primária, devido a relação de entidade-fraca
  *	codEd é uma chave primária dessa tabela que contém o código da edição, 
  diferenciando cada edição do evento das demais
  *	dataInicioEd representa a data inicial da edição
  *	dataFimEd representa a data de término da edição
- *	localEd representa o local onde o evento irá ocorrer (cidade, estado, endereço???? ***)
+ *	localEd representa o local onde o evento irá ocorrer no formato Cidade, País
  *	taxaEd representa o custo que os participantes devem pagar para
  participar da edição
  *	saldoFinanceiroEd é um atributo derivado que armazena a situação 
@@ -65,18 +64,21 @@ CREATE TABLE EVENTO (
  *	qtdArtigosApresentadosEd é um atributo derivado que armazena a quantidade
  total de artigos apresentados na edição
  */
-CREATE TABLE EDICAO (
-	codEv Number(10) NOT NULL,	-- Chave primária e estrangeira (identifica identidade fraca)
-	nomeEv Varchar2(15) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
+CREATE TABLE edicao (
+	-- criar id para passar para as outras tabelas??? ***
+	codEv Number(5) NOT NULL,	-- Chave primária e estrangeira (identifica identidade fraca)
+	nomeEv Varchar2(100) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
 	codEd Number(10) NOT NULL, -- Chave primária para diferenciar de evento
 	dataInicioEd Date,
 	dataFimEd Date,
-	localEd Varchar2(20), -- *** seria essa a melhor variável??? 
-	taxaEd Number(20),
+	localEd Varchar2(20), -- Cidade, País
+	taxaEd Number(20), -- Valor inteiro da taxa de inscrição
 	saldoFinanceiroEd Number(50), -- atributo derivado ***
-	qtdArtigosApresentadosEd Number(10) -- atributo derivado ***
+	qtdArtigosApresentadosEd Number(10), -- atributo derivado ***
 	-- contraints *** ???
-	CONSTRAINT PK_EDICAO PRIMARY KEY (codEv, nomeEv, codEd) -- PK_EDICAO define a restrição de chave primária
+	CONSTRAINT PK_EDICAO PRIMARY KEY (codEv, nomeEv, codEd), -- PK_EDICAO define a restrição de chave primária
+	CONSTRAINT FK_EVENTO_COD_EV FOREIGN KEY (codEv) REFERENCES evento(codEv) ON DELETE CASCADE,
+	CONSTRAINT FK_EVENTO_NOME_EV FOREIGN KEY (nomeEv) REFERENCES evento(nomeEv) ON DELETE CASCADE
 );
 /
 -- Criação da tabela de Patrocinador
@@ -90,18 +92,80 @@ CREATE TABLE EDICAO (
  *	enderecoPat que armazena o endereço da empresa em formato livre de texto 
  devidos aos diferentes países
  */
-CREATE TABLE PATROCINADOR (
-	cnpjPat	Number(14), -- Chave primária
+CREATE TABLE patrocinador (
+	cnpjPat	Number(14) NOT NULL, -- Chave primária
 	razaoSocialPat Varchar2(150), -- limite ??? ***
 	telefonePat Varchar2(16), -- +XX(XX)XXXX-XXXX *** ???
 	enderecoPat	Varchar(80),
 	-- constrains *** ???
 	CONSTRAINT PK_PATROCINADOR PRIMARY KEY (cnpjPat) -- PK_PATROCINADOR define a restrição de chave primária
-
 );
 /
--- Criação da tabela 
+-- Criação da tabela Patrocina
+/*
+ *
+ */
+CREATE TABLE patrocinio (
+	cnpjPat	Number(14) NOT NULL, -- Chave estrangeira e primária
+	codEv Number(5) NOT NULL,	-- Chave primária e estrangeira (identifica identidade fraca)
+	nomeEv Varchar2(100) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
+	codEd Number(10) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
 
+	dataPat Date,
+	valorPat Number(50),
+	saldoPat Number(100), -- atributo derivado do valor do patrocinio e o que foi gasto em 
+							-- auxílios ou despesas
+
+	CONSTRAINT PK_PATROCINIO PRIMARY KEY (cnpjPat, codEv, nomeEv, codEd),
+	CONSTRAINT FK_PATROCINADOR_CNPJ FOREIGN KEY (cnpjPat) REFERENCES patrocinador(cnpjPat),
+	CONSTRAINT FK_EDICAO_COD_EV FOREIGN KEY (codEv) REFERENCES edicao(codEv),
+	CONSTRAINT FK_EDICAO_NOME_EV FOREIGN KEY (nomeEv) REFERENCES edicao(nomeEv),
+	CONSTRAINT FK_EDICAO_COD_ED FOREIGN KEY (codEd) REFERENCES edicao(codEd)
+	-- precisa colocar que o valor tem que ser maior que zero??? ***
+);
+/
+-- Criação da tabela Despesa
+/*
+ *
+ */
+CREATE TABLE despesa (
+	codEv Number(5) NOT NULL,	-- Chave primária e estrangeira (identifica identidade fraca)
+	nomeEv Varchar2(100) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
+	codEd Number(10) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
+	codDesp Number(10) NOT NULL, -- Chave primária para diferenciar de evento
+
+	CONSTRAINT PK_DESPESA PRIMARY KEY (codEv, nomeEv, codEd, codDesp)
+);
+/
+-- Criação da tabela Pessoa
+/*
+ *
+ */
+CREATE TABLE PESSOA (
+	idPe Number(100),
+	emailPe Varchar2(250),
+
+	CONSTRAINT PK_PESSOA PRIMARY KEY (idPe, emailPe)
+);
+
+
+
+
+
+
+
+-- Criação da sequência para codEv da tabela evento
+CREATE SEQUENCE SEQ_CODEV_EVENTO
+	MINVALUE 0
+	START WITH 1
+	INCREMENT BY 1;
+/
+-- Criação da sequência para codEd da tabela edicao
+CREATE SEQUENCE SEQ_CODED_EDICAO
+	MINVALUE 0
+	START WITH 1
+	INCREMENT BY 1;
+/
 
 
 
--- scriptInsere.sql
+++ scriptInsere.sql
@@ -9,8 +9,71 @@
  * Script de Inserção < scriptInsere.sql >
  * 
  */
- /
- -- Inserção de Eventos
- -- add pelo menos 2
- /
- /
+/
+-- Inserção do primeiro Evento
+INSERT INTO evento(codEv, nomeEv, descricaoEv, websiteEv)
+	VALUES(
+	 	SEQ_CODEV_EVENTO.NEXTVAL, -- codigo do evento
+	 	'ACM International Conference on Information and Knowlege Managment', -- nome do evento
+	 	'CIKM is a top-tier conference sponsored by ACM in the areas of Information 
+	 	Retrieval, Knowledge Management and Databases, bringing together leading 
+	 	researchers and practitioners from the three communities to identify 
+	 	challenging problems facing the development of future knowledge and information 
+	 	systems, and to shape future research directions through the publication of high 
+	 	quality, applied and theoretical research findings.', -- descrição do evento
+	 	'http://cikm2014.fudan.edu.cn',-- website do evento
+);
+/
+-- Inserção do segundo Evento
+INSERT INTO evento(codEv, nomeEv, descricaoEv, websiteEv)
+	VALUES(
+	 	SEQ_CODEV_EVENTO.NEXTVAL, -- codigo do evento
+	 	'International Conference on Very Large Data Bases', -- nome do evento
+	 	'VLDB is a premier annual international forum for data management and database 
+	 	researchers, vendors, practitioners, application developers, and users. The 
+	 	conference will feature research talks, tutorials, demonstrations, and workshops. 
+	 	It will cover current issues in data management, database and information systems 
+	 	research. Data management and databases remain among the main technological 
+	 	cornerstones of emerging applications of the twenty-first century.', -- descrição do evento
+	 	'http://www.vldb.org/',-- website do evento
+);
+/
+-- Inserção do terceiro Evento
+INSERT INTO evento(codEv, nomeEv, descricaoEv, websiteEv)
+	VALUES(
+	 	SEQ_CODEV_EVENTO.NEXTVAL, -- codigo do evento
+	 	'Brazilian Symposium on Geoinformatics', -- nome do evento
+	 	'The GeoInfo series (Brazilian Symposium on Geoinformatics) is an annual conference 
+	 	for exploring ongoing research, development and innovative applications on geographic 
+	 	information science and related areas. Papers should describe original research, ongoing 
+	 	(preferably) or recently completed and are reviewed by an international scientific committee.
+		The GeoInfo conferences also aim to bring to Brazil leading GIScience and spatial database 
+		researchers, to present to the local community a perspective of the state-of-the-art in the 
+		area. Past speakers have included Max Egenhofer, Gary Hunter, Andrew Frank, Roger Bivand, 
+		Mike Worboys, Werner Kuhn, Stefano Spaccapietra, Ralf Guting, Shashi Shekhar, Christopher 
+		Jones, Martin Kulldorff, Andrea Rodriguez, Max Craglia, Stephen Winter, Edzer Pebesma, Fosca 
+		Giannotti, Christian Freksa, Thomas Bittner, and Markus Schneider.', -- descrição do evento
+	 	'http://www.geoinfo.info/',-- website do evento
+);
+/
+/
+/
+-- Inserção da edição de 2013 do primeiro evento
+INSERT INTO edicao(codEv, nomeEv, codEd, dataInicioEd, dataFimEd, localEd, taxaEd)
+	VALUES(
+		1, -- código do evento
+		'ACM International Conference on Information and Knowlege Managment', -- nome do evento
+		SEQ_CODED_EDICAO.NEXTVAL,
+		
+);
+
+
+codEv Number(10) NOT NULL,	-- Chave primária e estrangeira (identifica identidade fraca)
+	nomeEv Varchar2(15) NOT NULL, -- Chave primária e estrangeira (identifica identidade fraca)
+	codEd Number(10) NOT NULL, -- Chave primária para diferenciar de evento
+	dataInicioEd Date,
+	dataFimEd Date,
+	localEd Varchar2(20), -- *** seria essa a melhor variável??? 
+	taxaEd Number(20),
+
+
