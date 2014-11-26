930830318d3d8eb929ade8b5cbf79a8f6919dfab
Vanessa Lima <vanessalima@Vanessas-MacBook-Pro.local>
2014-11-26 05:30:36 -0200

    adicionando detalhes do gatilho

--- Trabalho05/scripts/scriptGatilhos.sql
+++ Trabalho05/scripts/scriptGatilhos.sql
@@ -27,18 +27,72 @@ END;
 /
 -- trigger para atualizar qntArtigosApresentadosEd
 	-- depende da inserção e deleção de artigos
+CREATE OR REPLACE TRIGGER quantidadeArtigosEdicao AFTER
+	INSERT OR DELETE ON artigo
+
+	for each ROW
+		--WHEN ... 
+		-- tem que usar para o delete
+		-- mais de uma pode ser deletada, mas só uma pode ser inserida
+
+	DECLARE
+
+	BEGIN
+
+END quantidadeArtigosEdicao;
+￼
 
 -- trigger para atualizar totalArtigosApresentadosEv
 	-- depende da atualização de qntArtigosApresentadosEd tabela edicao
+CREATE OR REPLACE TRIGGER quantidadeArtigosEvento AFTER
+	INSERT OR DELETE OR UPDATE OF qntArtigosApresentadosEd ON EDICAO
+
+	for each ROW
+		--WHEN ... 
+		-- tem que usar para o delete
+		-- mais de uma pode ser deletada, mas só uma pode ser inserida
+
+	DECLARE
+
+	BEGIN
+
+END quantidadeArtigosEvento;
+
 
 -- trigger para atualizar saldoPat
 	-- depende da:
 		-- inserção, remoção ou atualização do valorDesp tabela despesa
 		-- inserção, remoção ou atualização do valorAux tabela auxilio
+		-- NÃO SEI COMO FAZER DEPENDER DE MAIS DE 1 TABELA
+CREATE OR REPLACE TRIGGER novoSaldoPatrocinio AFTER
+	INSERT OR DELETE OR UPDATE OF valorDesp ON DESPESA
+
+	for each ROW
+		--WHEN ... 
+		-- tem que usar para o delete
+		-- mais de uma pode ser deletada, mas só uma pode ser inserida
+
+	DECLARE
+
+	BEGIN
+
+END novoSaldoPatrocinio;
 
 -- trigger para atualizar saldoFinanceiroEd
 	-- depende da inserção, remoção ou atualização (saldoPat) de patrocinio
+CREATE OR REPLACE TRIGGER novoSaldoEdicao AFTER
+	INSERT OR DELETE OR UPDATE OF saldoPat ON PATROCINIO
 
+	for each ROW
+		--WHEN ... 
+		-- tem que usar para o delete
+		-- mais de uma pode ser deletada, mas só uma pode ser inserida
+
+	DECLARE
+
+	BEGIN
+
+END novoSaldoEdicao;
 
 
 
